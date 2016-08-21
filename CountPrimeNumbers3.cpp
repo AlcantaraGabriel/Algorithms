afec2bcba3fc74d741fd0150f96705c68587934d
Gabriel Alcântara <gabrielalcantara39@gmail.com>
2016-08-20 23:29:50 -0300

    Create CountPrimeNumbers3.cpp

---
+++ CountPrimeNumbers3.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <vector>
+ 
+using namespace std;
+ 
+#define MAX 100000000000
+bool vet[MAX];
+ 
+vector <int> primos;
+ 
+int main()
+{
+    int n=100000000,NdivSeis = n/6;
+ 
+    for(int m=1; NdivSeis>=m; m++){
+        int x = 6*m -1,y = 6*m +1;
+ 
+        int NdivPrimo=n/x;
+ 
+        for(int j=6;(n/x)+1>=j;j+=6){
+ 
+            if(vet[x]==false || vet[y]==false){
+                if(vet[x]==false)
+                    vet[x*(j-1)]=true,vet[x*(j+1)]=true;
+                if(vet[y]==false)
+                    vet[y*(j-1)]=true,vet[y*(j+1)]=true;
+            }
+ 
+            else if(vet[x]==true && vet[y]==true)
+                break;
+        }
+ 
+        if(vet[x]==false){
+            //printf("%d\n",x);
+            primos.push_back(x);
+        }
+        if(vet[y]==false){
+            //printf("%d\n",y);
+            primos.push_back(y);
+        }
+    }
+    printf("Quantidade de primos: %d\n",primos.size()+2);
+    return 0;
+}
